ed23a421fac4a9ecc4004640e8ea22ecb39bd8a7
hanshuobest <hscoder@163.com>
2021-01-01 23:21:36 +0800

    2021-01-01 23:21

--- lidar_localization/include/lidar_localization/mapping/back_end/back_end.hpp
+++ lidar_localization/include/lidar_localization/mapping/back_end/back_end.hpp
@@ -18,31 +18,33 @@
 
 #include "lidar_localization/models/graph_optimizer/g2o/g2o_graph_optimizer.hpp"
 
-namespace lidar_localization {
-class BackEnd {
+namespace lidar_localization
+{
+  class BackEnd
+  {
   public:
     BackEnd();
 
-    bool Update(const CloudData& cloud_data, const PoseData& laser_odom, const PoseData& gnss_pose);
-    bool InsertLoopPose(const LoopPose& loop_pose);
+    bool Update(const CloudData &cloud_data, const PoseData &laser_odom, const PoseData &gnss_pose);
+    bool InsertLoopPose(const LoopPose &loop_pose);
     bool ForceOptimize();
 
-    void GetOptimizedKeyFrames(std::deque<KeyFrame>& key_frames_deque);
+    void GetOptimizedKeyFrames(std::deque<KeyFrame> &key_frames_deque);
     bool HasNewKeyFrame();
     bool HasNewOptimized();
-    void GetLatestKeyFrame(KeyFrame& key_frame);
-    void GetLatestKeyGNSS(KeyFrame& key_frame);
+    void GetLatestKeyFrame(KeyFrame &key_frame);
+    void GetLatestKeyGNSS(KeyFrame &key_frame);
 
   private:
     bool InitWithConfig();
-    bool InitParam(const YAML::Node& config_node);
-    bool InitGraphOptimizer(const YAML::Node& config_node);
-    bool InitDataPath(const YAML::Node& config_node);
+    bool InitParam(const YAML::Node &config_node);
+    bool InitGraphOptimizer(const YAML::Node &config_node);
+    bool InitDataPath(const YAML::Node &config_node);
 
     void ResetParam();
-    bool SavePose(std::ofstream& ofs, const Eigen::Matrix4f& pose);
-    bool AddNodeAndEdge(const PoseData& gnss_data);
-    bool MaybeNewKeyFrame(const CloudData& cloud_data, const PoseData& laser_odom, const PoseData& gnss_pose);
+    bool SavePose(std::ofstream &ofs, const Eigen::Matrix4f &pose);
+    bool AddNodeAndEdge(const PoseData &gnss_data);
+    bool MaybeNewKeyFrame(const CloudData &cloud_data, const PoseData &laser_odom, const PoseData &gnss_pose);
     bool MaybeOptimized();
     bool SaveOptimizedPose();
 
@@ -67,32 +69,34 @@ class BackEnd {
     // 优化器
     std::shared_ptr<InterfaceGraphOptimizer> graph_optimizer_ptr_;
 
-    class GraphOptimizerConfig {
-      public:
-        GraphOptimizerConfig() {
-          odom_edge_noise.resize(6);
-          close_loop_noise.resize(6);
-          gnss_noise.resize(3);
-        }
+    class GraphOptimizerConfig
+    {
+    public:
+      GraphOptimizerConfig()
+      {
+        odom_edge_noise.resize(6);
+        close_loop_noise.resize(6);
+        gnss_noise.resize(3);
+      }
 
-      public:
-        bool use_gnss = true;
-        bool use_loop_close = false;
+    public:
+      bool use_gnss = true;
+      bool use_loop_close = false;
 
-        Eigen::VectorXd odom_edge_noise;
-        Eigen::VectorXd close_loop_noise;
-        Eigen::VectorXd gnss_noise;
+      Eigen::VectorXd odom_edge_noise;
+      Eigen::VectorXd close_loop_noise;
+      Eigen::VectorXd gnss_noise;
 
-        int optimize_step_with_key_frame = 100;
-        int optimize_step_with_gnss = 100;
-        int optimize_step_with_loop = 10;
+      int optimize_step_with_key_frame = 100;
+      int optimize_step_with_gnss = 100;
+      int optimize_step_with_loop = 10;
     };
     GraphOptimizerConfig graph_optimizer_config_;
 
     int new_gnss_cnt_ = 0;
     int new_loop_cnt_ = 0;
     int new_key_frame_cnt_ = 0;
-};
-}
+  };
+} // namespace lidar_localization
 
 #endif
