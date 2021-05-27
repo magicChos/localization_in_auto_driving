42df1d7686fc7e7edb1c9266fe10d1b53832263a
hanshuo <hanshuo@seedland.cc>
2021-05-27 14:00:16 +0800

    add update

--- lidar_localization/include/lidar_localization/tools/file_manager.hpp
+++ lidar_localization/include/lidar_localization/tools/file_manager.hpp
@@ -10,12 +10,14 @@
 #include <iostream>
 #include <fstream>
 
-namespace lidar_localization {
-class FileManager{
+namespace lidar_localization
+{
+  class FileManager
+  {
   public:
-    static bool CreateFile(std::ofstream& ofs, std::string file_path);
+    static bool CreateFile(std::ofstream &ofs, std::string file_path);
     static bool CreateDirectory(std::string directory_path);
-};
+  };
 }
 
 #endif
--- lidar_localization/src/tools/file_manager.cpp
+++ lidar_localization/src/tools/file_manager.cpp
@@ -8,25 +8,31 @@
 #include <boost/filesystem.hpp>
 #include "glog/logging.h"
 
-namespace lidar_localization {
-bool FileManager::CreateFile(std::ofstream& ofs, std::string file_path) {
-    ofs.open(file_path.c_str(), std::ios::app);
-    if (!ofs) {
-        LOG(WARNING) << "无法生成文件: " << file_path;
-        return false;
+namespace lidar_localization
+{
+    bool FileManager::CreateFile(std::ofstream &ofs, std::string file_path)
+    {
+        ofs.open(file_path.c_str(), std::ios::app);
+        if (!ofs)
+        {
+            LOG(WARNING) << "无法生成文件: " << file_path;
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-}
-
-bool FileManager::CreateDirectory(std::string directory_path) {
-    if (!boost::filesystem::is_directory(directory_path)) {
-        boost::filesystem::create_directory(directory_path);
+    bool FileManager::CreateDirectory(std::string directory_path)
+    {
+        if (!boost::filesystem::is_directory(directory_path))
+        {
+            boost::filesystem::create_directory(directory_path);
+        }
+        if (!boost::filesystem::is_directory(directory_path))
+        {
+            LOG(WARNING) << "无法建立文件夹: " << directory_path;
+            return false;
+        }
+        return true;
     }
-    if (!boost::filesystem::is_directory(directory_path)) {
-        LOG(WARNING) << "无法建立文件夹: " << directory_path;
-        return false;
-    }
-    return true;
-}
 }
