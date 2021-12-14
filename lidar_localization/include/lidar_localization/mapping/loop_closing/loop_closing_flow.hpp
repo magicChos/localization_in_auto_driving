27f4471b4351ab795eb457724705c40fbf98de12
hanshuo <hanshuo@seedland.com>
2021-12-14 18:05:13 +0800

    loop detect

--- lidar_localization/include/lidar_localization/mapping/loop_closing/loop_closing_flow.hpp
+++ lidar_localization/include/lidar_localization/mapping/loop_closing/loop_closing_flow.hpp
@@ -1,5 +1,5 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Ren Qian
  * @Date: 2020-02-29 03:32:14
  */
@@ -15,34 +15,36 @@
 // loop closing
 #include "lidar_localization/mapping/loop_closing/loop_closing.hpp"
 
-namespace lidar_localization {
-class LoopClosingFlow {
-  public:
-    LoopClosingFlow(ros::NodeHandle& nh);
+namespace lidar_localization
+{
+    class LoopClosingFlow
+    {
+    public:
+        LoopClosingFlow(ros::NodeHandle &nh);
 
-    bool Run();
+        bool Run();
 
-  private:
-    bool ReadData();
-    bool HasData();
-    bool ValidData();
-    bool PublishData();
+    private:
+        bool ReadData();
+        bool HasData();
+        bool ValidData();
+        bool PublishData();
 
-  private:
-    // subscriber
-    std::shared_ptr<KeyFrameSubscriber> key_frame_sub_ptr_;
-    std::shared_ptr<KeyFrameSubscriber> key_gnss_sub_ptr_;
-    // publisher
-    std::shared_ptr<LoopPosePublisher> loop_pose_pub_ptr_;
-    // loop closing
-    std::shared_ptr<LoopClosing> loop_closing_ptr_;
+    private:
+        // subscriber
+        std::shared_ptr<KeyFrameSubscriber> key_frame_sub_ptr_;
+        std::shared_ptr<KeyFrameSubscriber> key_gnss_sub_ptr_;
+        // publisher
+        std::shared_ptr<LoopPosePublisher> loop_pose_pub_ptr_;
+        // loop closing
+        std::shared_ptr<LoopClosing> loop_closing_ptr_;
 
-    std::deque<KeyFrame> key_frame_buff_;
-    std::deque<KeyFrame> key_gnss_buff_;
+        std::deque<KeyFrame> key_frame_buff_;
+        std::deque<KeyFrame> key_gnss_buff_;
 
-    KeyFrame current_key_frame_;
-    KeyFrame current_key_gnss_;
-};
+        KeyFrame current_key_frame_;
+        KeyFrame current_key_gnss_;
+    };
 }
 
 #endif
